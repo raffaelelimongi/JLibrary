75746e09fa3979497bca87d246120f303880a1ac
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-06-04 15:53:22 +0200

    new java class

---
+++ src/vo/ImmagineDati.java
@@ -0,0 +1,19 @@
+package vo;
+
+public class ImmagineDati
+{
+    String nomeimg;
+
+    public ImmagineDati(String nomeimg)
+    {
+        this.nomeimg=nomeimg;
+    }
+
+    public String getNomeimg() {
+        return nomeimg;
+    }
+
+    public void setNomeimg(String nomeimg) {
+        this.nomeimg = nomeimg;
+    }
+}
