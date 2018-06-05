b0b9b13ae80fcc5cf18e57396a8786bafd30eab0
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-06-06 01:42:40 +0200

    eliminate ridondanza query

--- src/dao/SearchOperaQuery.java
+++ src/dao/SearchOperaQuery.java
@@ -11,17 +11,6 @@ public class SearchOperaQuery implements SearchOperaInterface
     {
 
     }
-    public ResultSet SearchOperaQueryKeyword(String keyword) throws SQLException
-    {
-        ConnectionClass connectionClass = new ConnectionClass();
-        Connection connection = connectionClass.getConnection();
-        String sql ="SELECT titolo,autore,c.nome FROM opera join categoria c ON (opera.IDcategoria=c.ID) WHERE TITOLO LIKE ?";
-        ps = connection.prepareStatement(sql);
-        ps.setString(1,"%"+keyword+"%");
-        ResultSet resultSet = ps.executeQuery();
-        return resultSet;
-    }
-
     public ResultSet SearchOperaQueryGeneral(String keyword, String kind) throws SQLException
     {
         ConnectionClass connectionClass = new ConnectionClass();
@@ -41,3 +30,5 @@ public class SearchOperaQuery implements SearchOperaInterface
     }
 
 }
+
+
