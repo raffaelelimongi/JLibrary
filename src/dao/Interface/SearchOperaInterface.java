0ee604abf9a04c00afd74c8497a6f3ad39514f1b
IcArtur <39270462+IcArtur@users.noreply.github.com>
2018-06-06 13:38:49 +0200

    Delete SearchOperaInterface.java

--- src/dao/Interface/SearchOperaInterface.java
+++
@@ -1,9 +0,0 @@
-package dao.Interface;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface SearchOperaInterface
-{
-    ResultSet SearchOperaQueryGeneral(String keyword, String kind) throws SQLException;
-}
