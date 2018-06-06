97bf116ffdca97877afeed0d50fb385b54aca754
IcArtur <39270462+IcArtur@users.noreply.github.com>
2018-06-06 13:39:23 +0200

    Add files via upload
    Aggiunto metodo keyword

---
+++ src/dao/Interface/SearchOperaInterface.java
@@ -0,0 +1,10 @@
+package dao.Interface;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface SearchOperaInterface
+{
+     ResultSet SearchOperaQueryKeyword(String keyword) throws SQLException;
+     ResultSet SearchOperaQueryGeneral(String keyword, String kind) throws SQLException;
+}
