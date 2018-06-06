368c428cf6a90700c99315ae013ddb0345abca9b
IcArtur <39270462+IcArtur@users.noreply.github.com>
2018-06-06 13:38:26 +0200

    Add files via upload

---
+++ src/dao/SearchOperaQuery.java
@@ -0,0 +1,50 @@
+package dao;
+
+        import dao.Interface.SearchOperaInterface;
+
+        import java.sql.*;
+
+public class SearchOperaQuery implements SearchOperaInterface
+{
+    PreparedStatement ps;
+    public void SearchOperaQuery()
+    {
+
+    }
+    @Override
+    public ResultSet SearchOperaQueryKeyword(String keyword) throws SQLException {
+
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+        Statement statement = connection.createStatement();
+
+        //preparo la query da inviare ed eseguire sul DB
+        String sql = "SELECT titolo,autore,c.nome FROM opera join categoria c ON (opera.IDcategoria=c.ID) WHERE TITOLO LIKE'%" + keyword + "%'" ;
+
+        //ritorno il sisultato della query
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        return resultSet;
+    }
+
+
+    public ResultSet SearchOperaQueryGeneral(String keyword, String kind) throws SQLException
+    {
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+
+        String sql = "SELECT" + " titolo,autore,c.nome, IDcategoria " +
+                "FROM opera join categoria c ON (opera.IDcategoria=c.ID) " +
+                "WHERE TITOLO LIKE ?" +
+                "AND c.nome LIKE ?";
+        ps = connection.prepareStatement(sql);
+        ps.setString(1,"%"+keyword+"%");
+        ps.setString(2,"%"+kind+"%");
+
+        ResultSet resultSet = ps.executeQuery();
+
+        return resultSet;
+    }
+
+}
+
