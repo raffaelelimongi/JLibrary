69ab28875c0a29b5f11d71b12e8e6c315e402031
IcArtur <39270462+IcArtur@users.noreply.github.com>
2018-06-07 17:25:50 +0200

    bona

---
+++ src/dao/Interface/UserAuthenticationInterface.java
@@ -0,0 +1,10 @@
+package dao.Interface;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface UserAuthenticationInterface
+{
+    ResultSet SingInAdminQuery(String username1, String password1) throws SQLException;
+    int SignInQuery(String user, String pass, String nome, String cognome, String email) throws SQLException;
+    ResultSet UserAuthenticationQuery(String username1,String password1) throws SQLException;
+}
