b07e90aff6180e2d7b4b163d6c1f179f0c837e89
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-06-10 02:34:58 +0200

    Update InfoUserTable.java

--- src/model/InfoUserTable.java
+++ src/model/InfoUserTable.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package model;
 
 import vo.UserInfo;
@@ -33,74 +31,4 @@ public class InfoUserTable extends UserInfo
     public void setPrivilegio(String privilegio) {
         this.privilegio = privilegio;
     }
-=======
-package model;
-
-import vo.UserInfo;
-
-import java.io.IOException;
-
-public class InfoUserTable extends UserInfo
-{
-    String username,privilegio;
-
-    public InfoUserTable(String username, String privilegio,String nome,String cognome,String email) throws IOException
-    {
-        super(nome,cognome,email);
-        this.username=username;
-        this.privilegio=privilegio;
-
-    }
-    public String getUsername()
-    {
-        return username;
-    }
-
-    public void setUsername(String username)
-    {
-        this.username = username;
-    }
-
-    public String getPrivilegio() {
-        return privilegio;
-    }
-    public void setPrivilegio(String privilegio) {
-        this.privilegio = privilegio;
-    }
->>>>>>> 361e359ca24548f535b8284958b62647770df092
-=======
-package model;
-
-import vo.UserInfo;
-
-import java.io.IOException;
-
-public class InfoUserTable extends UserInfo
-{
-    String username,privilegio;
-
-    public InfoUserTable(String username, String privilegio,String nome,String cognome,String email) throws IOException
-    {
-        super(nome,cognome,email);
-        this.username=username;
-        this.privilegio=privilegio;
-
-    }
-    public String getUsername()
-    {
-        return username;
-    }
-
-    public void setUsername(String username)
-    {
-        this.username = username;
-    }
-
-    public String getPrivilegio() {
-        return privilegio;
-    }
-    public void setPrivilegio(String privilegio) {
-        this.privilegio = privilegio;
-    }
->>>>>>> 361e359ca24548f535b8284958b62647770df092
 }
