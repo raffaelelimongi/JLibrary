7dddfb927c97c66779374b4b73b3b006da0467cf
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-06-12 16:50:18 +0200

    Update for JavaFXController

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,20 +2,14 @@ package controller;
 
 import dao.*;
 import dao.Interface.UserInfoInterface;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import java.io.IOException;
 import java.sql.*;
 
-public class LoginController extends Application
+public class LoginController
 {
     @FXML
     public TextField textusername,textpassword;
@@ -30,15 +24,8 @@ public class LoginController extends Application
     {
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/Login.fxml"));
-        primaryStage.setTitle("JLibrary");
-        primaryStage.setScene(new Scene(root, 300, 285));
-        primaryStage.show();
-    }
     
-    public void Login (ActionEvent event) throws Exception
+    public void Login (ActionEvent event)
     {
         try {
             UserAuthenticationQuery userDaoInterface = new UserAuthenticationQuery();
@@ -49,7 +36,7 @@ public class LoginController extends Application
             if(resultSet.next())
             {
                 userInfoInterface.UserInfoQuery(textusername.getText());
-                new HomePageController().setscene(event);
+                new JavaFXController().sethome(event);
             }
             else {
                 result.setText("Username or password invalid");
@@ -59,18 +46,8 @@ public class LoginController extends Application
         }
     }
 
-    public void gotosignup(ActionEvent event) throws IOException
+    public void gotosignup(ActionEvent event)
     {
-        Stage stage;
-        Parent home;
-
-        stage = (Stage) btsignup.getScene().getWindow();
-        home = FXMLLoader.load(getClass().getResource("../view/signup.fxml"));
-
-        Scene homescene = new Scene(home);
-        stage.setScene(homescene);
-        stage.setTitle("SignUp");
-
-        stage.show();
+        JavaFXController.setSignUp(event);
     }
 }
