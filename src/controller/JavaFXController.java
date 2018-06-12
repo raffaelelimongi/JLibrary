1dc1bcc4026f15a88d782cab29d369a00e88ae2f
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-06-12 16:43:28 +0200

    Upload JavaFXController

---
+++ src/controller/JavaFXController.java
@@ -0,0 +1,220 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class JavaFXController extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/Login.fxml"));
+        primaryStage.setTitle("JLibrary");
+        primaryStage.setScene(new Scene(root, 300, 285));
+        primaryStage.show();
+    }
+
+    public static void sethome(ActionEvent event) {
+        Parent root;
+        try {
+            //setto la nuova scena della home page e nascondo la precedente
+            root = FXMLLoader.load(HomePageController.class.getResource("../view/home.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("HomePage");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setLogin(ActionEvent event) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(ViewOperaController.class.getResource("../view/Login.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setSignUp(ActionEvent event)
+    {
+        Parent root;
+        try {
+            root = FXMLLoader.load(JavaFXController.class.getResource("../view/signup.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("SignUp");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setAdminPanel(ActionEvent event)
+    {
+        Parent root1;
+        try {
+            root1 = FXMLLoader.load(JavaFXController.class.getResource("../view/AdminPanel.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("AdminPanel");
+            Scene home = new Scene(root1);
+            stage.setScene(home);
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setProfile(ActionEvent event)
+    {
+        Parent root;
+        try {
+            root = FXMLLoader.load(JavaFXController.class.getResource("../view/profile.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Profile");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setUpload()
+    {
+        Parent root;
+        try {
+            root = FXMLLoader.load(JavaFXController.class.getResource("../view/uploadView.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Chose Image");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setManageUser(ActionEvent event)
+    {
+        Parent root;
+        try {
+            root = FXMLLoader.load(JavaFXController.class.getResource("../view/gestioneuser.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Manage User");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setTrascrizione(ActionEvent event)
+    {
+        Parent root;
+        try
+        {
+            root = FXMLLoader.load(JavaFXController.class.getResource("../view/trascrizione.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Trascrizione");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setManageImage()
+    {
+        Parent root;
+        try {
+            root = FXMLLoader.load(JavaFXController.class.getResource("../view/imageview.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Manage Image");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setAssegnaTrascrizione()
+    {
+        Parent root;
+        try
+        {
+            root = FXMLLoader.load(JavaFXController.class.getResource("../view/assegnatrascrizione.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Trascrizione");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setTeiEditor() throws IOException
+    {
+        // Setto la scena per l'apertura della finestra del teiEditor di quella determinata opera
+        Parent root;
+
+        root = FXMLLoader.load(JavaFXController.class.getResource("../view/teieditor.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Trascrizione");
+        Scene home = new Scene(root);
+        stage.setScene(home);
+        stage.show();
+    }
+
+    public static void setViewOpera()
+    {
+        Parent root;
+        try
+        {
+            root = FXMLLoader.load(ViewOperaController.class.getResource("../view/operaview.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Opera");
+            Scene home = new Scene(root);
+            stage.setScene(home);
+            stage.show();
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
