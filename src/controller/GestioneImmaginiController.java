83cd19bfe11991955cc1cdf50fbfd61d7c52b6b9
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-07-01 18:19:07 +0200

    Update GestioneImmaginiController.java

--- src/controller/GestioneImmaginiController.java
+++ src/controller/GestioneImmaginiController.java
@@ -1,26 +1,22 @@
 package controller;
 
+import dao.ImageQuery;
+import dao.Interface.ImageQueryInterface;
 import dao.Interface.OperaInfoInterface;
 import dao.OperaInfoQuery;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 import model.ImmagineDati;
-import java.io.IOException;
+import model.OperaMetadati;
 import java.net.URL;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class GestioneImmaginiController implements Initializable
@@ -32,45 +28,24 @@ public class GestioneImmaginiController implements Initializable
 
     private ObservableList<ImmagineDati> oblist;
 
-    public String titolo;
-
     OperaInfoInterface operainfo = new OperaInfoQuery();
+    ImageQueryInterface listimage = new ImageQuery();
+
+    ArrayList<OperaMetadati> listopere;
+    ArrayList<ImmagineDati> imagelist;
 
     public GestioneImmaginiController()
     {}
-
-    public void setscene(ActionEvent event)
+    public void gotohome(ActionEvent event)
     {
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("../view/gestioneimage.fxml"));
-            Stage stage = new Stage();
-            stage.setTitle("Manage Image");
-            Scene home = new Scene(root);
-            stage.setScene(home);
-            stage.show();
-            ((Node) (event.getSource())).getScene().getWindow().hide();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        JavaFXController.sethome(event);
     }
 
- /*   public void gotohome(ActionEvent event)
-    {
-        HomePageController.setscene(event);
-    }*/
-
     //metodo per caricare il testo dal DB al Text
-    public void LoadInfoOpera() throws SQLException, IOException
+    public void LoadInfoOpera() throws SQLException
     {
-        ResultSet resultSet = operainfo.LoadOpera(titolo);  //faccio la query per caricarmi le immagini da revisionare
-
-        while (resultSet.next())
-        {
-            titolo = resultSet.getString("titolo");
-            setTable(titolo, null);
-        }
+        listopere = operainfo.LoadOpera();  //faccio le query per caricarmi le info delle opere relative alle immagini da revisionare
+        setTable(listopere);
     }
 
     @Override
@@ -85,15 +60,17 @@ public class GestioneImmaginiController implements Initializable
             LoadInfoOpera();
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
     //Metodo per settare la Tableview con i valori presi dal DB
 
-    private void setTable(String titolo, ImageView imageView2) throws IOException
+    private void setTable(ArrayList<OperaMetadati> listop) throws SQLException
     {
-        oblist.add(new ImmagineDati("",imageView2,titolo,"","",null));
-        tablemanage.setItems(oblist);
+        for(int i=0;i<listop.size();i++)
+        {
+            imagelist= listimage.LoadImage(listop.get(i).getTitolo());
+            oblist.add(new ImmagineDati("",null, imagelist.get(i).getImageView(), listop.get(i).getTitolo(), "", "", null));
+            tablemanage.setItems(oblist);
+        }
     }
 }
