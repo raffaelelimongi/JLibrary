17341df2d34df17a4daa7acc96e1f40970e39b20
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-07-01 18:20:46 +0200

    Update OperaInfoInterface.java

--- src/dao/Interface/OperaInfoInterface.java
+++ src/dao/Interface/OperaInfoInterface.java
@@ -5,14 +5,15 @@ import model.OperaMetadati;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public interface OperaInfoInterface
 {
-    int OperaInfoQuery(String titolo, String autore, LocalDate data, String genere, String nomeimmagine, String path) throws SQLException;
+    int OperaInfoQuery(OperaMetadati opera) throws SQLException;
 
     void DeleteOpera(OperaMetadati delopera) throws SQLException;
 
     void UploadImageQuery(String nome, String path,String tit) throws SQLException;
 
-    ResultSet LoadOpera(String tit) throws SQLException;
+    ArrayList<OperaMetadati> LoadOpera() throws SQLException;
 }
