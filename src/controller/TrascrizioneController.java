1bec8ae25ae3ecbd45c3cd71a082d6d69adabadb
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-07-01 17:45:08 +0200

    Update TrascrizioneController.java

--- src/controller/TrascrizioneController.java
+++ src/controller/TrascrizioneController.java
@@ -12,10 +12,9 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.UserModel;
 import model.TrascrizioneDati;
-import java.io.IOException;
 import java.net.URL;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class TrascrizioneController implements Initializable
@@ -31,11 +30,8 @@ public class TrascrizioneController implements Initializable
 
     TrascrizioneQueryInterface trascrQueryInterface = new TrascrizioneQuery();
 
-    String titolo;
-
-    TrascrizioneDati trascrizioneDati= new TrascrizioneDati(titolo,null,null);
-
     UserModel userModel =UserModel.getInstance();
+    ArrayList<TrascrizioneDati> listtrasc= new ArrayList<>();
 
     public TrascrizioneController()
     {
@@ -47,33 +43,21 @@ public class TrascrizioneController implements Initializable
     }
 
     //in questo metodo carico il titolo dell'opera da trascrivere nella tabella per poterlo visualizzare
-    public void getTitolo() throws SQLException, IOException
+    public void getTitolo() throws SQLException
     {
         oblist.removeAll(oblist);
-
-        ResultSet resultSet = trascrQueryInterface.getTrascrizioniList(userModel.getUsername(),userModel.getPrivilegio());
-
         if(userModel.getPrivilegio().equals("supervisor") || userModel.getPrivilegio().equals("admin"))
         {
-            ResultSet result = trascrQueryInterface.getUserAbility(); //effettuo la query per selezionare tutte le trascrizioni aperte nel caso in cui l'utente è un supervisor.
-            while (result.next())
-            {
-                titolo = result.getString("o.titolo");
-                setTable(titolo);
-                trascrizioneDati.setTitolo(titolo);
+                listtrasc = trascrQueryInterface.getTrascUserAbility(); //effettuo la query per selezionare tutte le trascrizioni aperte nel caso in cui l'utente è un supervisor.
+                setTable(listtrasc);
             }
-        }else {
-            while (resultSet.next()) {
-                //controllo se la trascrizione è stata accettata o meno, se non è stata accettata la carico nella tabella
-                if (!resultSet.getBoolean("op.accept")) {
-                    //setto le variabili con le informazioni presenti nel DB e le passo al metodo setTable
-                    titolo = resultSet.getString("titolo");
-                    setTable(titolo);
-                    trascrizioneDati.setTitolo(titolo);
-                }
-            }
+        else {
+
+            listtrasc = trascrQueryInterface.getTrascrizioniList(userModel);
+            setTable(listtrasc);
         }
     }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
@@ -87,14 +71,15 @@ public class TrascrizioneController implements Initializable
             getTitolo();
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
     //Metodo per settare la Tableview con i valori presi dal DB
-    private void setTable(String titolo)
+    private void setTable(ArrayList<TrascrizioneDati>trasclist)
     {
-        oblist.add(new TrascrizioneDati(titolo,null,null));
-        tabletrascrizioni.setItems(oblist);
+        for(int i=0;i<trasclist.size();i++)
+        {
+            oblist.add(trasclist.get(i));
+            tabletrascrizioni.setItems(oblist);
+        }
     }
 }
