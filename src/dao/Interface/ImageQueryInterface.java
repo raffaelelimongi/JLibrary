0090790296b63745d0ea783905c86d6d7e8130bc
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-07-01 18:19:58 +0200

    Update ImageQueryInterface.java

--- src/dao/Interface/ImageQueryInterface.java
+++ src/dao/Interface/ImageQueryInterface.java
@@ -1,16 +1,21 @@
 package dao.Interface;
 
+import model.ImmagineDati;
+import model.OperaMetadati;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface ImageQueryInterface
 {
-    ResultSet LoadImage(String tit) throws SQLException;
+    ArrayList<ImmagineDati> LoadImage(String tit) throws SQLException;
 
-    int UploadImageQuery(String nome, String path,String tit,String autore) throws SQLException;
+    int UploadImageQuery(ImmagineDati image, OperaMetadati opera) throws SQLException;
 
-    void Accept(String name, String tit) throws SQLException;
+    void Accept(ArrayList<ImmagineDati> listimage, String tit) throws SQLException;
 
-    void Decline(String name,String titolo) throws SQLException;
+    void Decline(String titolo) throws SQLException;
+
+     ArrayList<ImmagineDati> LoadImageOpera(String tit) throws SQLException;
 
 }
