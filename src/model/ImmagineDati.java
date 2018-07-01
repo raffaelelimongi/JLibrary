55b9826e91fc97523cbbad481e6c06751c4ea1f2
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-07-01 18:21:15 +0200

    Update ImmagineDati.java

--- src/model/ImmagineDati.java
+++ src/model/ImmagineDati.java
@@ -1,40 +1,34 @@
 package model;
 
 import controller.JavaFXController;
-import controller.ViewImage;
-import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import model.OperaMetadati;
-
-import java.io.IOException;
 import java.util.Date;
+
 
 public class ImmagineDati extends OperaMetadati
 {
     private ImageView imageView;
+    private String path;
     String nomeimg;
     private Label link;
 
-    public ImmagineDati(String nomeimg,ImageView imageView,String t,String a,String g,Date d) throws IOException
+    public ImmagineDati(String nomeimg,String path, ImageView imageView, String t, String a, String g, Date d)
     {
         super(t,a,g,d);
         this.nomeimg=nomeimg;
+        this.path=path;
         this.imageView=imageView;
         this.link =new Label("view");
         link.setUnderline(true);
         link.setStyle("-fx-background-color: red");
 
         //se viene cliccato apre la finestra per la visualizzazione dell'opera
-        link.setOnMouseClicked((new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-              //  new ViewImage().setscene(t);
-
-                JavaFXController.setManageImage();
-            }
-    }   ));
+        link.setOnMouseClicked((MouseEvent mouseEvent) ->
+        {
+            JavaFXController.setViewImage(t);
+        });
     }
 
     public String getNomeimg() {
@@ -60,5 +54,12 @@ public class ImmagineDati extends OperaMetadati
     public void setLink(Label link) {
         this.link = link;
     }
-}
 
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
