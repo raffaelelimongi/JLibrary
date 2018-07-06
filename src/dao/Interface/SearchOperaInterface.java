9d64ca331f5dcb449455f9611a073a6d9b55b6c2
Raffaele Limongi <38720448+klaus97@users.noreply.github.com>
2018-07-06 11:10:13 +0200

    Update SearchOperaInterface.java

--- src/dao/Interface/SearchOperaInterface.java
+++ src/dao/Interface/SearchOperaInterface.java
@@ -8,12 +8,9 @@ import java.util.ArrayList;
 
 public interface SearchOperaInterface
 {
-    ArrayList<OperaMetadati> SearchOperaQueryGeneral(String keyword, String kind) throws SQLException;
+    ArrayList<OperaMetadati> SearchOperaQueryGeneral(String ricerca, String kind) throws SQLException;
 
     ArrayList<OperaMetadati> LoadOpera(String tit) throws SQLException;
 
-    ArrayList<OperaMetadati> SearchOperaQueryAdmin(String keyword) throws SQLException;
-
-    ResultSet SearchOperaSoft() throws SQLException;
+    ArrayList<OperaMetadati> SearchOperaQueryAdmin(String ricerca) throws SQLException;
 }
-
